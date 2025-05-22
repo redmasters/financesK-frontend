11d8e20d75890801181cb93bc62764bfcc8c1d1d
Erasmo Jr. <erasmo.cjunior@gmail.com>
2025-05-22 01:39:44 -0300

    chore: server rendering

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,16 @@ import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
 import {provideClientHydration, withEventReplay} from '@angular/platform-browser';
 import {provideHttpClient} from '@angular/common/http';
+import {provideServerRouting} from '@angular/ssr';
+import {serverRoutes} from './app.routes.server';
+import {provideServerRendering} from '@angular/platform-server';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
+    provideServerRouting(serverRoutes),
+    provideServerRendering(),
     provideHttpClient(),
     provideClientHydration(withEventReplay())
   ]
