8c73a03307fd8eb17a5a0a3c81aa8d44a38eba8e
Erasmo Jr. <erasmo.cjunior@gmail.com>
2025-05-23 01:07:02 -0300

    chore: added environments

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import { environment } from '../../environments/environment'
 
 export interface Category {
   id: number;
@@ -44,7 +45,7 @@ export interface SpendResponse {
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = "http://localhost:8080/api";
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {
   }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://164.152.35.186:8080/api',
+}
